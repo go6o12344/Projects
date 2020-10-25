f84f0d503b399e4c4b2cfcf784fd05d419daa27d
go6o12344 <72763513+go6o12344@users.noreply.github.com>
2020-10-25 15:45:01 +0200

    Update fps.py

--- python/doomclone/fps.py
+++ python/doomclone/fps.py
@@ -5,7 +5,7 @@ import math
 import keyboard
 
 #Some random RGB colors
-SHADOW = (192, 192, 192)
+SHADOW = (102, 102, 102)
 WHITE = (255, 255, 255)
 LIGHTGREEN = (0, 255, 0 )
 GREEN = (0, 200, 0 )
@@ -40,8 +40,8 @@ clock = pygame.time.Clock()
 
 #Player class
 class Player:
-	x = 1.000
-	y = 1.000
+	x = 4.500
+	y = 4.500
 	AOV = 0
 	def rotate(self,dir):
 		self.AOV+= dir * pi*sensitivity
@@ -59,7 +59,7 @@ player = Player()
 #This is the game map
 map = []
 map.append([1,1,1,1,1,1,1,1,1,1])
-map.append([1,8,0,0,1,0,0,0,0,1])
+map.append([1,0,0,0,1,0,0,0,0,1])
 map.append([1,0,1,0,1,1,0,1,1,1])
 map.append([1,0,0,0,1,0,0,1,0,1])
 map.append([1,0,0,1,0,0,0,1,0,1])
@@ -71,11 +71,13 @@ map.append([1,1,1,1,1,1,1,1,1,1])
 
 #Find Cartesian equation of a ray
 def findEq(slopeAngle,x,y):
-	
+	t = math.tan(slopeAngle)
+	return(t, y - x * t)
 	
 #Find intersection of two lines using their equations in Cartesian
-def intersect(a,b,c,d):
-	return ((b-d)/(c-a), (a*b+c*b-a*d-c*d)/(c-a))
+#def intersect(a,b):
+#	return ((a[1]-b[1])/(b[0]-a[0]), (a[0]*a[1]+b[0]*a[1]-a[0]*b[1]-b[0]*b[1])/(b[0]-a[0]))
+
 #Draw starting screen
 def startingScreen():
 	global game_over
@@ -108,22 +110,63 @@ def startingScreen():
 #Draw what player sees		
 def drawVision(player, map):
 	screen.fill(WHITE)
-	for i in range(0,61):
+	for i in range(0,46):
 		#Ray casting
-		for j in range(1,3):
-			wallx = math.floor(player.y+j*math.sin(player.AOV+i*pi/180))
-			wally = math.floor(player.x+j*math.cos(player.AOV+i*pi/180))
-			pygame.draw.rect(screen, SHADOW, (600-i*10,0,10,750))
-			if map[wally][wallx]==1:
-				if(intersect(
-					pygame.draw.rect(screen, (128*math.sqrt((player.y-wallx)**2+(player.x-wally)**2),0,128*math.sqrt((player.y-wallx)**2+(player.x-wally)**2)), (600-i*10,0,10,750))
+		currAngle = player.AOV + i*pi/180
+		if currAngle != pi/2 and currAngle != -pi/2:
+			slope = math.tan(currAngle)
+		for j in range(1,7):
+			print(1)
+			wally = math.floor(player.y+j/2*math.sin(player.AOV-i*pi/180))
+			wallx = math.floor(player.x+j/2*math.cos(player.AOV-i*pi/180))
+			pygame.draw.rect(screen, SHADOW, (600-i*20,0,20,750))
+			if map[math.floor(wally)][math.floor(wallx)]==1:
+				if currAngle != pi/2 and currAngle!= -pi/2 and currAngle != 0 and currAngle != pi: 
+					#intersect ray with y = wally
+					pair1 = (player.x + (wally  - player.y)/slope, wally )
+					#intersect ray with x = wallx + 1
+					pair2 = (wallx + 1, slope * (wallx + 1 - player.x) + player.y)
+					print('pair1 ', pair1)
+					print('wallx ', wallx)
+					if pair1[0] <= wallx or pair1[0]>=wallx+1: pair1 = pair2
+				elif currAngle != pi/2 or currAngle!= -pi/2: pair1 = (wallx, player.y)
+				else: pair1 = (player.x, wally)
+				print()
+				print('pair1 ', pair1)
+				print('currAngle ', currAngle)
+				print('dist ', math.sqrt((player.y-pair1[1])**2+(player.x-pair1[0])**2))
+				print('slope ', slope)
+				print()
+				dist = math.sqrt((player.y-pair1[1])**2+(player.x-pair1[0])**2)
+				pygame.draw.rect(screen, (228-50*dist,0,228-50*dist), (600+i*20-10,0,20,750))
 				break
-		for j in range(1,3):
-			wallx = math.floor(player.y+j*math.sin(player.AOV-i*pi/180))
-			wally = math.floor(player.x+j*math.cos(player.AOV-i*pi/180))
-			pygame.draw.rect(screen, SHADOW, (610+i*10,0,10,750))
-			if map[wally][wallx]==1:
-				pygame.draw.rect(screen, (128*math.sqrt((player.y-wallx)**2+(player.x-wally)**2),0,128*math.sqrt((player.y-wallx)**2+(player.x-wally)**2)), (610+i*10-10,0,10,750))
+		currAngle = player.AOV - i*pi/180
+		if currAngle != pi/2 and currAngle != -pi/2:
+			slope = math.tan(currAngle)
+		for j in range(1,7):
+			print(1)
+			wally = math.floor(player.y+j/2*math.sin(player.AOV-i*pi/180))
+			wallx = math.floor(player.x+j/2*math.cos(player.AOV-i*pi/180))
+			pygame.draw.rect(screen, SHADOW, (600+i*20,0,20,750))
+			if map[math.floor(wally)][math.floor(wallx)]==1:
+				if currAngle != pi/2 and currAngle!= -pi/2 and currAngle != 0 and currAngle != pi: 
+					#intersect ray with y = wally
+					pair1 = (player.x + (wally  - player.y)/slope, wally )
+					#intersect ray with x = wallx + 1
+					pair2 = (wallx + 1, slope * (wallx + 1 - player.x) + player.y)
+					print('pair1 ', pair1)
+					print('wallx ', wallx)
+					if pair1[0] <= wallx or pair1[0]>=wallx+1: pair1 = pair2
+				elif currAngle != pi/2 or currAngle!= -pi/2: pair1 = (wallx, player.y)
+				else: pair1 = (player.x, wally)
+				print()
+				print('pair1 ', pair1)
+				print('currAngle ', currAngle)
+				print('dist ', math.sqrt((player.y-pair1[1])**2+(player.x-pair1[0])**2))
+				print('slope ', slope)
+				print()
+				dist = math.sqrt((player.y-pair1[1])**2+(player.x-pair1[0])**2)
+				pygame.draw.rect(screen, (228-50*dist,0,228-50*dist), (600+i*20-10,0,20,750))
 				break
 	pygame.display.flip()
 startingScreen()
@@ -136,7 +179,7 @@ while running:
 		break
 	if keyboard.is_pressed('q'):
 		player.rotate(1)
-	elif keyboard.is_pressed('e'):
+	if keyboard.is_pressed('e'):
 		player.rotate(-1)
 	drawVision(player,map)
 	clock.tick(15)
